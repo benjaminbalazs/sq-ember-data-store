b0de95e68c3a04f981f07aee7e36b88e88f67605
Benjamin Balazs <benjaminbalazs@gmail.com>
2015-10-03 22:48:06 +0400

    fix

--- addon/mixins/saver.js
+++ addon/mixins/saver.js
@@ -3,7 +3,7 @@ import Ember from 'ember';
 export default Ember.Mixin.create({
 
 	save(model, param) {
-
+		
 		var self = this;
 
         return new Ember.RSVP.Promise(function(resolve, reject) {
