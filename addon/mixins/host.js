8bec509aa72a2679f8b7f0af9074767775feb841
benjaminbalazs@gmail.com <benjaminbalazs@Benjamins-iMac-2.local>
2017-04-28 12:51:04 +0400

    trace

--- addon/mixins/host.js
+++ addon/mixins/host.js
@@ -1,4 +1,5 @@
 import Ember from 'ember';
+import config from 'ember-get-config';
 
 export default Ember.Mixin.create({
 
@@ -6,16 +7,25 @@ export default Ember.Mixin.create({
 
     getHost() {
 
-        var headers = this.get('fastboot.request.headers');
-        console.log(headers);
-        var host = headers.get('x-original-host');
-        var protocol = headers.get('x-original-protocol');
+        //var headers = this.get('fastboot.request.headers');
+        //console.log(headers);
+        //var host = headers.get('x-original-host');
+        //var protocol = headers.get('x-original-protocol');
 
-        if ( host.indexOf('.com') !== -1 ) {
-            protocol = "https";
+        //if ( host.indexOf('.com') !== -1 ) {
+        //    protocol = "https";
+        //}
+
+        var host = config.APP.protocol + config.APP.domain;
+
+        if ( config.fastboot.api_namespace ) {
+            console.log(host + '/' + config.fastboot.api_namespace);
+            return host + '/' + config.fastboot.api_namespace;
+
+        } else {
+            console.log(host);
+            return host;
         }
-
-        return protocol + '://' + host;
 
     }
 
