630b5935bbd261c2243fde0500a534379b5a926a
benjaminbalazs@gmail.com <benjaminbalazs@Benjamins-iMac-2.local>
2017-04-28 22:24:04 +0400

    cleanupx

--- addon/mixins/host.js
+++ addon/mixins/host.js
@@ -7,23 +7,11 @@ export default Ember.Mixin.create({
 
     getHost() {
 
-        //var headers = this.get('fastboot.request.headers');
-        //console.log(headers);
-        //var host = headers.get('x-original-host');
-        //var protocol = headers.get('x-original-protocol');
-
-        //if ( host.indexOf('.com') !== -1 ) {
-        //    protocol = "https";
-        //}
-
         var host = config.APP.protocol + config.APP.domain;
 
         if ( config.fastboot.api_namespace ) {
-            console.log(host + '/' + config.fastboot.api_namespace);
             return host + '/' + config.fastboot.api_namespace;
-
         } else {
-            console.log(host);
             return host;
         }
 
