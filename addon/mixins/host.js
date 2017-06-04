db00c32e20625e9ea91812becbfd3c8db24637a2
benjaminbalazs@gmail.com <benjaminbalazs@Benjamins-iMac-2.local>
2017-06-04 10:00:49 +0400

    ref

--- addon/mixins/host.js
+++ addon/mixins/host.js
@@ -7,21 +7,29 @@ export default Ember.Mixin.create({
 
     getHost() {
 
-        var host = config.APP.protocol + config.APP.domain;
-
-        var headers = this.get('fastboot.request.headers');
-        var realHost = headers.get('X-Real-Host');
-        var realScheme = headers.get('X-Real-Scheme');
-
-        if ( realHost && realScheme ) {
-            host = realScheme + '://' + realHost;
-        }
+        var host = this.getDomain();
 
         if ( config.fastboot.api_namespace ) {
             return host + '/' + config.fastboot.api_namespace;
         } else {
             return host;
         }
+
+    },
+
+    getDomain() {
+
+        var host = config.APP.protocol + config.APP.domain;
+
+        var headers = this.get('fastboot.request.headers');
+        var realHost = headers.get('X-Real-Host');
+        var realScheme = headers.get('X-Real-Scheme');
+    
+        if ( realHost && realScheme ) {
+            host = realScheme + '://' + realHost;
+        }
+
+        return host;
 
     }
 
