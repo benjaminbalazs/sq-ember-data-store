b98e6bf4e573daf94fbc7f1d239fae2636566243
benjaminbalazs@gmail.com <benjaminbalazs@Benjamins-iMac-2.local>
2017-07-23 16:18:14 +0400

    request

--- app/services/request.js
+++ app/services/request.js
@@ -133,9 +133,9 @@ export default Ember.Service.extend(Host,{
         if ( response.status >= 200 && response.status < 300 ) {
             return response;
         } else {
-            var error = new Error(response.statusText);
-            error.response = response;
-            throw error;
+            return response.json().then(function(result) {
+                throw result;
+            });
         }
 
     },
