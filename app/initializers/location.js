8ce92b3e337601ddb3da752343a79acf840f6137
benjaminbalazs@gmail.com <benjaminbalazs@Benjamins-iMac-2.local>
2017-12-04 18:26:32 +0400

    ct

--- app/initializers/location.js
+++ app/initializers/location.js
@@ -4,6 +4,7 @@ export function initialize (app) {
 
 	app.register('service:location', Service);
 	app.inject('route', 'location', 'service:location');
+	app.inject('controller', 'location', 'service:location');
     app.inject('component', 'location', 'service:location');
 
 }
