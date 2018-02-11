7bce4370c4db4355eccaa424db6d2c1235f203d6
Benjamin Balazs <benjaminbalazs@gmail.com>
2018-02-11 14:46:33 +0400

    fb

--- addon/adapters/application.js
+++ addon/adapters/application.js
@@ -4,16 +4,23 @@ import config from 'ember-get-config';
 
 export default DS.JSONAPIAdapter.extend({
 
+    location: Ember.inject.service(),
+    fastboot: Ember.inject.service(),
+
     init() {
 
 		this._super();
 
 		this.set('namespace', config.APP.api_namespace);
 
+        if ( this.get('fasboot.isFastBoot') === true ) {
+            this.set('host', this.get('location.domain'));
+        }
+
 	},
 
     headers: Ember.computed('session.headers.Authorization', 'session.headers.Socket', function() {
-    
+
         return this.get('session.headers');
 
     }),
